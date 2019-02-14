fca752defd65e9b891fcd82240afa7502673c03f
UmbertoB <umbertobarrosf@gmail.com>
2019-02-14 14:21:28 -0300

    [master] | Configuring Spotify API environment

---
+++ src/app/services/interceptor/token.interceptor.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { SpotifyApiService } from 'src/app/services/spotify-api.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(public auth: SpotifyApiService) {
+  }
+
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (this.auth.getToken()) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.auth.getToken()}`
+        }
+      });
+    }
+
+    return next.handle(request).pipe(tap(
+    (event: HttpEvent<any>) => {
+      
+
+      if (event instanceof HttpResponse) {
+
+        if (event.body && event.body.error) {
+          throw(event);
+        }
+
+      }
+
+    },
+    (error: any) => {}));
+  }
+}
